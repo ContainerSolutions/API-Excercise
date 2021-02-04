5ec70b9f84758a54c2459a2d0ce7242d00d3e7d1
Spazzy <brendankamp757@gmail.com>
2021-02-04 11:24:29 +0100

    Added wait

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ kubectl apply -f k8s_manifests/01-ns.yaml
 kubectl apply -f k8s_manifests/backend/
 kubectl apply -f k8s_manifests/storage/
 
-sleep 10
+kubectl -n titanic wait --for=condition=ready --timeout=600s pod -l app=postgres
+kubectl -n titanic wait --for=condition=ready --timeout=600s pod -l app=titanic
 
-kubectl -n titanic get pods
