202fa10957b70b2afd6dc42fd030370299185adb
Spazzy <brendankamp757@gmail.com>
2021-02-04 13:36:50 +0100

    Updated DB

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,8 @@ kubectl apply -f k8s_manifests/storage/
 
 sleep 30
 
-kubectl -n titanic describe pod -l app=postgres
 kubectl -n titanic wait --for=condition=ready --timeout=300s pod -l app=postgres
+kubectl -n titanic describe pod -l app=titanic
+kubectl -n titaninc logs -l  app=titanic
 kubectl -n titanic wait --for=condition=ready --timeout=300s pod -l app=titanic
 
